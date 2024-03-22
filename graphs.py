7e98c17c89d2ebb64ce56e954e5d53b1735c1f7f
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-03-21 21:48:04 -0300

    start

---
+++ graphs.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+
+
+def generate_graph(complete_data: pd.DataFrame):
+
+    # Custom colorscale
+    custom_colorscale = [
+        [0, "rgba(217, 217, 217, 1)"],  # grey for 0 count
+        [1.0 / complete_data["count"].max(), "#ffeda0"],  # light orange for min count
+        [1, "#ff4500"],  # dark orange for max count
+    ]
+    # Create a base map to show all country borders
+    fig = go.Figure(
+        data=go.Choropleth(
+            locations=complete_data["country"],
+            z=complete_data["count"],
+            locationmode="country names",
+            colorscale=custom_colorscale,
+            marker_line_color="black",  # Lines between countries
+            marker_line_width=0.5,
+            colorbar_title="Number of Authors",
+        )
+    )
+    fig.update_geos(
+        projection_type="orthographic",
+        showcountries=True,  # show country boundaries
+    )
+    # Update the layout to add the title and adjust geo settings
+    fig.update_layout(
+        title_text="Number of Authors by Country",
+        coloraxis_colorbar=dict(
+            title="Number of Authors",
+            tickvals=[
+                0,
+                complete_data["count"].max(),
+            ],  # ensures that 0 is labeled on the colorbar
+            ticktext=["0", "Max"],
+        ),
+        geo=dict(
+            showframe=False,
+            showcoastlines=False,
+            projection_type="equirectangular",
+            landcolor="rgba(217, 217, 217, 1)",  # Light grey land color
+            lakecolor="rgba(127,205,255,1)",  # Light blue lake color
+            oceancolor="rgba(127,205,255,0.5)",  # Slightly transparent ocean color
+        ),
+    )
+
+    fig.show()
+
+
+if __name__ == "__main__":
+    # Sample data
+    country_counts = pd.DataFrame(
+        {
+            "country": [
+                "United States",
+                "United Kingdom",
+                "France",
+                "Germany",
+                "Canada",
+                "Australia",
+                "Brazil",
+            ],
+            "count": [20, 15, 10, 8, 5, 3, 2],
+        }
+    )
+
+    generate_graph(country_counts)
